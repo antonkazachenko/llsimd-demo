f4754d15d9a03585c1c3f55c80e94fc511288e3d
creamlaflare <antonkazachenko2002@gmail.com>
2025-03-28 14:09:45 -0700

    create header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import styles from './header.module.css';
+import { ReactComponent as GithubLogo } from '../../assets/images/githubLogo.svg';
 
 function Header() {
   return (
     <div className={styles.headerBlock}>
-      <div className={styles.header}>Header</div>
+      <div className={styles.header}>
+        <div className={styles.logoText}>
+          LLVM Transformation Pass
+        </div>
+        <div>|</div>
+        <div className={styles.subText}>
+          Save/Load
+        </div>
+        <div className={styles.subText}>
+          A&darr;
+        </div>
+        <div>|</div>
+        <div className={styles.subText}>
+          About
+        </div>
+        <GithubLogo />
+      </div>
     </div>
   );
 }
