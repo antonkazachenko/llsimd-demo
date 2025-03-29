7ef4fce7d08f94e2eb91278f9e7efc13134b5ca7
creamlaflare <antonkazachenko2002@gmail.com>
2025-03-29 15:30:14 -0700

    add btn

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,12 +5,19 @@ import CodeInput from '../code-input/code-input';
 function Main() {
   return (
     <div className={styles.main}>
-      <div className={styles.inputBlock}>
-        <CodeInput />
+      <div className={styles.codeBlock}>
+        <div className={styles.inputBlock}>
+          <CodeInput />
+        </div>
+        <div className={styles.outputBlock}>
+          <div className={styles.firstOutput}><CodeInput /></div>
+          <div className={styles.secondOutput}><CodeInput /></div>
+        </div>
       </div>
-      <div className={styles.outputBlock}>
-        <div className={styles.firstOutput}><CodeInput /></div>
-        <div className={styles.secondOutput}><CodeInput /></div>
+      <div className={styles.btnStyle}>
+        <span className={styles.btnText}>
+          Submit Code
+        </span>
       </div>
     </div>
   );
